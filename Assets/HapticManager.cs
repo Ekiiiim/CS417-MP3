82c7ba336b66c0c607fafef13c41bf3625d9adf7
Mike Cheng <minyuc2@illinois.edu>
2026-04-02 19:24:47 -0500

    improve haptics

--- Assets/HapticManager.cs
+++ Assets/HapticManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
@@ -25,6 +26,36 @@ public class HapticManager : MonoBehaviour
     public void OnNewRegionUnlock()
     {
         SendHaptic(rightController, 0.8f, 0.3f);
+        SendHaptic(leftController, 0.8f, 0.3f);
+    }
+
+    public void OnCoffeeShopUnlock()
+    {
+        SendHaptic(rightController, 0.8f, 0.3f);
+    }
+
+    public void OnSupermarketUnlock()
+    {
+        SendHaptic(rightController, 0.8f, 0.3f);
+    }
+
+    public void OnStadiumUnlock()
+    {
+        SendHaptic(rightController, 0.8f, 0.3f);
+    }
+
+    public void OnCropPowerUpActivate()
+    {
+        SendDoubleHaptic(0.7f, 0.1f);
+    }
+
+    private IEnumerator SendDoubleHaptic(float amplitude, float duration)
+    {
+        SendHaptic(rightController, amplitude, duration);
+        SendHaptic(leftController, amplitude, duration);
+        yield return new WaitForSeconds(0.1f);
+        SendHaptic(rightController, amplitude, duration);
+        SendHaptic(leftController, amplitude, duration);
     }
 
     private void SendHaptic(HapticImpulsePlayer controller, float amplitude, float duration)
