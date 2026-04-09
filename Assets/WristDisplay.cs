c7da378ea266cffc54388c6e38296c558a3c758e
Mike Cheng <minyuc2@illinois.edu>
2026-04-08 21:14:11 -0500

    juicy tutorial pop-up

--- Assets/WristDisplay.cs
+++ Assets/WristDisplay.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
 using System;
+using System.Collections;
 
 public class WristDisplay : MonoBehaviour
 {
@@ -11,11 +12,18 @@ public class WristDisplay : MonoBehaviour
     public GameObject CityWall;
     public InputActionReference toggleCanvasAction;
 
+    public Vector3 targetScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
+    public float stiffness = 150f;
+    public float damping = 10f;
+    private float currentScale, velocity;
+    private bool isAnimating = false;
     private bool showTutorial = false;
 
     void Start()
     {
         toggleCanvasAction.action.performed += ToggleCanvasVisibility;
+        transform.localScale = Vector3.zero;
+        TriggerPopIn();
     }
 
     private void OnDestroy()
@@ -25,26 +33,41 @@ public class WristDisplay : MonoBehaviour
 
     private void ToggleCanvasVisibility(InputAction.CallbackContext context)
     {
-        if (showTutorial) {
-            showTutorial = false;
-            InfoText.fontSize = 16f;
-            TitleText.text = "Status Board";
-            HintText.text = "Press X to close/open";
-        } else {
-            gameObject.SetActive(!gameObject.activeSelf);
+        if (showTutorial)
+        {
+            ResetToStatusBoard();
         }
+        else
+        {
+            if (!gameObject.activeSelf) TriggerPopIn();
+            else gameObject.SetActive(false);
+        }
+    }
+
+    private void ResetToStatusBoard()
+    {
+        showTutorial = false;
+        InfoText.fontSize = 16f;
+        TitleText.text = "Status Board";
+        HintText.text = "Press X to close/open";
     }
 
     void Update()
     {
-        if (showTutorial) return;
+        if (isAnimating) HandleSpringScale();
+        if (showTutorial || !gameObject.activeSelf) return;
 
+        UpdateStatusText();
+    }
+
+    private void UpdateStatusText()
+    {
         float cropCount = ResourceManager.Instance.cropCount;
         float cropGrowthRate = ResourceManager.Instance.cropGrowthRate;
         float totalUpkeep = ResourceManager.Instance.houseUpkeepPerSecond;
         float cropBonusMulti = ResourceManager.Instance.curPowerUpMulti;
 
-        string infoString = $"Crops: {cropCount:N0}   (+{cropBonusMulti*(cropGrowthRate - totalUpkeep):F2}/sec)\n";
+        string infoString = $"Crops: {cropCount:N0}   (+{cropBonusMulti * (cropGrowthRate - totalUpkeep):F2}/sec)\n";
         if (!CityWall.activeSelf)
         {
             float electricityCount = ResourceManager.Instance.electricityCount;
@@ -59,12 +82,36 @@ public class WristDisplay : MonoBehaviour
         InfoText.text = infoString;
     }
 
+    private void TriggerPopIn()
+    {
+        currentScale = 0f;
+        velocity = 0f;
+        gameObject.SetActive(true);
+        isAnimating = true;
+    }
+
+    private void HandleSpringScale()
+    {
+        float force = (1f - currentScale) * stiffness;
+        velocity += force * Time.deltaTime;
+        velocity -= velocity * damping * Time.deltaTime;
+        currentScale += velocity * Time.deltaTime;
+        transform.localScale = targetScale * currentScale;
+
+        if (Mathf.Abs(1f - currentScale) < 0.001f && Mathf.Abs(velocity) < 0.001f)
+        {
+            transform.localScale = targetScale;
+            isAnimating = false;
+        }
+    }
+
     public void ShowHouseTutorial()
     {
         showTutorial = true;
         TitleText.text = "Tutorial: Houses";
         InfoText.text = "House residents help you collect crops, but houses require upkeep!\n\nUpgrade houses to exponentially increase residents' efficiency.";
         HintText.text = "Press X to dismiss";
+        TriggerPopIn();
     }
 
     public void ShowCityTutorial()
